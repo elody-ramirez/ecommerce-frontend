0c26f10623f59df66482475accbf6741707f8499
elody-ramirez <elody.ramirez@gmail.com>
2022-12-27 23:16:46 -0500

    added the increase and decrease qty functionality for items, also it add to cart works

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,72 +3,85 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { Product } from '../../components'
 import { client, urlFor } from '../../lib/client'
+import { useStateContext } from '../../context/StateContext'
 
 const ProductDetails = ({ product, products }) => {
-  const [index, setIndex] = useState(0)
   const { image, name, details, price } = product
+  const [index, setIndex] = useState(0)
+  const { decQty, incQty, qty, onAdd } = useStateContext()
 
   return (
-    <div>
-      <div className="product-detail-container">
-        <div>
-          <div className="image-container">
-            <img src={urlFor(image && image[index])} className="product-detail-image"/>
-          </div>
-          <div className="small-images-container">
-            {image?.map((item, i) => (
-              <img
-                key={item._id}
-                src={urlFor(item)} 
-                className={`small-image ${i === index ? 'selected-image' : ''}`}
-                onMouseEnter={() => setIndex(i)}
-              />
-            ))}
-          </div>
-        </div>
+		<div>
+			<div className="product-detail-container">
+				<div>
+					<div className="image-container">
+						<img
+							src={urlFor(image && image[index])}
+							className="product-detail-image"
+						/>
+					</div>
+					<div className="small-images-container">
+						{image?.map((item, i) => (
+							<img
+								key={item._id}
+								src={urlFor(item)}
+								className={`small-image ${i === index ? "selected-image" : ""}`}
+								onMouseEnter={() => setIndex(i)}
+							/>
+						))}
+					</div>
+				</div>
 
-        <div className="product-detail-desc">
-          <h1>{name}</h1>
-          <div className="reviews">
-            <div>
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiFillStar />
-              <AiOutlineStar />
-            </div>
-            <p>(20)</p>
-          </div>
-          <h4>Details: </h4>
-          <p>{details}</p>
-          <p className="price">{price}</p>
-          <div className="quantity">
-            <h3>Quantity:</h3>
-            <p className="quantity-desc">
-              <span className="minus"><AiOutlineMinus /></span>
-              <span className="num">0</span>
-              <span className="plus"><AiOutlinePlus /></span>
-            </p>
-          </div>
-          <div className="buttons">
-            <button type="button" className="add-to-cart">Add to Cart</button>
-            <button type="button" className="buy-now">Buy Now</button>
-          </div>
-        </div>
-      </div>
+				<div className="product-detail-desc">
+					<h1>{name}</h1>
+					<div className="reviews">
+						<div>
+							<AiFillStar />
+							<AiFillStar />
+							<AiFillStar />
+							<AiFillStar />
+							<AiOutlineStar />
+						</div>
+						<p>(20)</p>
+					</div>
+					<h4>Details: </h4>
+					<p>{details}</p>
+					<p className="price">{price}</p>
+					<div className="quantity">
+						<h3>Quantity:</h3>
+						<p className="quantity-desc">
+							<span className="minus" onClick={decQty}>
+								<AiOutlineMinus />
+							</span>
+							<span className="num">{qty}</span>
+							<span className="plus" onClick={incQty}>
+								<AiOutlinePlus />
+							</span>
+						</p>
+					</div>
+					<div className="buttons">
+						<button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>
+							Add to Cart
+						</button>
+						<button type="button" className="buy-now">
+							Buy Now
+						</button>
+					</div>
+				</div>
+			</div>
 
-      <div className="maylike-products-wrapper">
-        <h2>You may also like</h2>
-        <div className="marquee">
-          <div className="maylike-products-container track">
-            {products.map((item) => (
-              <Product key={item._id} product={item}/>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+			<div className="maylike-products-wrapper">
+				<h2>You may also like</h2>
+				<div className="marquee">
+					<div className="maylike-products-container track">
+						{products.map((item) => (
+							<Product key={item._id} product={item} />
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export const getStaticPaths = async () => {
