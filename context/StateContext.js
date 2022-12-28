2dc0bf8b78a22920cf6423f5627a95bf55e43c37
elody-ramirez <elody.ramirez@gmail.com>
2022-12-28 11:45:47 -0500

    Corrected updating the toggleCartItemQuantity to maintain the same order of items

--- context/StateContext.js
+++ context/StateContext.js
@@ -48,18 +48,17 @@ export const StateContext = ({ children }) => {
   const toggleCartItemQuantity = (id, value) => {
     foundProduct = cartItems.find((item) => item._id === id)
     index = cartItems.findIndex((product) => product._id === id)
-    const newCartItems = cartItems.filter((item) => item._id !== id)
+    const newCartItems = [...cartItems]
 
     if (value === 'inc') {
-      setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }])
+      newCartItems.find((item) => item === foundProduct).quantity += 1
+      setCartItems(newCartItems)
       setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
       setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
     } else if (value === 'dec') {
       if (foundProduct.quantity > 1) {
-        setCartItems([
-					...newCartItems,
-					{ ...foundProduct, quantity: foundProduct.quantity - 1 },
-				])
+        newCartItems.find((item) => item === foundProduct).quantity -= 1
+				setCartItems(newCartItems)
 				setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
 				setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1)
       }
